c97ab9690f49c955f394dc5947a6a23122074e06
Marcin Natanek <mnatan@openmailbox.org>
2016-04-01 13:17:20 +0200

    Moved backend link setting to rest module #16

--- lib/CharonFront/App.pm
+++ lib/CharonFront/App.pm
@@ -1,34 +1,28 @@
 package CharonFront::App;
 use Dancer2;
 use Dancer2::Core::Request;
-use Dancer2::Plugin::Deferred;
 use Dancer2::Core::Role::ConfigReader;
-
-BEGIN {    # FIXME dirty hack for annoying include errors
-    use FindBin;
-    use Cwd qw/realpath/;
-    my $appdir = realpath("$FindBin::Bin/..");
-    unshift @INC, $appdir;
-}
-
-use CharonFront::RESTUser qw(json_post json_get);
-
-use FindBin;
-use Cwd qw/realpath/;
-use YAML::XS qw/LoadFile/;
+use Dancer2::Plugin::Deferred;
 
 use Data::Dumper;
 use feature qw/say/;
 
+use FindBin;
+use Cwd qw/realpath/;
+use lib realpath("$FindBin::Bin/..");
+
+use YAML::XS qw/LoadFile/;
+
+use CharonFront::RESTUser qw(backend_post backend_get);
+
 our $VERSION = '0.1';
-my $DEBUG              = 1;
-my $BACKEND_SERVER_URL = setting("BACKEND_SERVER_URL");
+my $DEBUG = 1;
 
 my $appdir = realpath("$FindBin::Bin/..");
 my $forms  = LoadFile("$appdir/configs/forms.yml");
 
 get '/' => sub {
-    my $registrations = json_get( $BACKEND_SERVER_URL . "/registrations" );
+    my $registrations = backend_get("/registrations");
     template 'index', { registrations => $registrations };
 };
 
@@ -38,15 +32,14 @@ post '/login' => sub {
     my $pass       = param('password');
     my $return_url = param('return_url') // '/';
 
-    my $backend_auth
-        = json_post( $BACKEND_SERVER_URL . "/authorize", scalar params );
+    my $backend_auth = backend_post( "/authorize", scalar params );
 
     unless ( defined $backend_auth->{token} ) {
         deferred error => "Niepoprawny użytkownik lub hasło!";
         redirect $return_url;
     }
 
-    my $user_data = json_get($BACKEND_SERVER_URL . "/user/$login");
+    my $user_data = backend_get("/user/$login");
 
     session login     => $login;
     session token     => $backend_auth->{token};
@@ -60,7 +53,7 @@ post '/login' => sub {
 
 get '/logout' => sub {
     my $return_url = param('return_url') // '/';
-	
+
     deferred info => "Pomyślnie wylogowano użytkownika: " . session 'user';
     app->destroy_session;
     redirect $return_url;
@@ -80,9 +73,8 @@ post '/register' => sub {
         redirect '/register';
     }
 
-    #FIXME json_post umiera z braku contentu w ramce http
-    my $backend_registration
-        = json_post( "$BACKEND_SERVER_URL/users/register", $submitted );
+    #FIXME backend_post umiera z braku contentu w ramce http
+    my $backend_registration = backend_post( "/users/register", $submitted );
 
     if ( $backend_registration->{status} eq 500 ) {
         deferred error => $backend_registration->{exception};
@@ -103,7 +95,7 @@ get '/userlist' => sub {
     my $return_url = param('return_url') // '/';
     my $users;
     if ( session "logged_in" ) {
-        $users = json_get( "$BACKEND_SERVER_URL/users",
+        $users = backend_get( "/users",
             { login => session("login"), token => session("token") } );
     }
     template "userlist", { users => $users };
@@ -112,8 +104,6 @@ get '/userlist' => sub {
 hook before_template => sub {
     my $tokens = shift;
 
-    # just use [% backend_url %] in tt files
-    $tokens->{backend_url}    = uri_for($BACKEND_SERVER_URL);
     $tokens->{register_url}   = uri_for('/register');
     $tokens->{logout_url}     = uri_for('/logout');
     $tokens->{login_url}      = uri_for('/login');
--- lib/CharonFront/RESTUser.pm
+++ lib/CharonFront/RESTUser.pm
@@ -2,64 +2,42 @@ package CharonFront::RESTUser;
 use strict;
 use warnings;
 
-use Exporter qw(import);
-our @EXPORT_OK = qw(
-    json_post
-    json_get
-);
-
 use LWP::UserAgent;
 use HTTP::Request;
 use JSON;
+
+use Dancer2;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(
+    backend_post
+    backend_get
+);
 
 use Data::Dumper;
 
 my $DEBUG = 1;
 
-my $user_agent = "Charon-Frontend";
+my $user_agent         = "Charon-Frontend";
+my $BACKEND_SERVER_URL = setting("BACKEND_SERVER_URL");
 
-sub json_post {
+sub backend_get {
     my ( $url, $arguments ) = @_;
     my $out;
 
-	print "--------- POST -----------\n" if $DEBUG;
-    print Dumper $arguments if $DEBUG;
+    print "--------- GET -----------\n" if $DEBUG;
 
     my $ua = LWP::UserAgent->new;
     $ua->agent($user_agent);
 
-    my $response = $ua->post( $url, $arguments );
-
-    if ( $response->content ) {
-        $out = decode_json $response->content;
-    }
-    else {
-        $out = {
-            status  => $response->code,
-            message => $response->message,
-        };
-    }
-    print Dumper $out if $DEBUG;
-	print "--------  /POST  ----------\n" if $DEBUG;
-    return $out;
-}
-
-sub json_get {
-    my ( $url, $arguments ) = @_;
-    my $out;
-
-	print "--------- GET -----------\n" if $DEBUG;
-
-    my $ua = LWP::UserAgent->new;
-    $ua->agent($user_agent);
-
-    # Pass a url sanitazier
     my @parameters;
     while ( my ( $key, $value ) = each %{$arguments} ) {
         push @parameters, "$key=$value";
     }
     my $parameters_for_url = join "&", @parameters;
-    my $response = $ua->get( $url . "?$parameters_for_url" );
+
+    my $response
+        = $ua->get( $BACKEND_SERVER_URL . $url . "?$parameters_for_url" );
 
     if ( $response->content ) {
         $out = decode_json $response->content;
@@ -71,7 +49,33 @@ sub json_get {
         };
     }
     print Dumper $out if $DEBUG;
-	print "--------  /GET  ----------\n" if $DEBUG;
+    print "--------  /GET  ----------\n" if $DEBUG;
+    return $out;
+}
+
+sub backend_post {
+    my ( $url, $arguments ) = @_;
+    my $out;
+
+    print "--------- POST -----------\n" if $DEBUG;
+    print Dumper $arguments if $DEBUG;
+
+    my $ua = LWP::UserAgent->new;
+    $ua->agent($user_agent);
+
+    my $response = $ua->post( $BACKEND_SERVER_URL . $url, $arguments );
+
+    if ( $response->content ) {
+        $out = decode_json $response->content;
+    }
+    else {
+        $out = {
+            status  => $response->code,
+            message => $response->message,
+        };
+    }
+    print Dumper $out if $DEBUG;
+    print "--------  /POST  ----------\n" if $DEBUG;
     return $out;
 }
 
